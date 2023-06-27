0b27b51f875a20859298b9654b2bdbde234bb486
Yullia90 <y.tsarenko@harwind.com.ua>
2023-06-28 01:16:50 +0300

    add slider card

--- scripts/script.js
+++ scripts/script.js
@@ -75,3 +75,29 @@ document.getElementById('change-currency').onclick = function (e) {
       +(prices[i].getAttribute('data-base-price') * coefficient).toFixed(1) + ' ' + newCurrency;
   }
 };
+
+$(document).ready(function () {
+  $('.slider').slick({
+    arrows: true,
+    dots: true,
+    slidesToShow: 1,
+    // autoplay: true,
+    // speed: 1000,
+    // autoplaySpeed: 800,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
+
