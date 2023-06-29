5fea18e4b6dc54c4a0ef5f6b5c6f9807c15f9109
Yullia90 <y.tsarenko@harwind.com.ua>
2023-06-29 17:50:38 +0300

    add

--- scripts/script.js
+++ scripts/script.js
@@ -155,38 +155,38 @@ document.addEventListener('click', function (event) {
 });
 
 
-function zoomImage(image) {
-  // Створюємо модальне вікно для збільшення картинки
-  var modalImageCard = document.createElement('div');
-  modalImageCard.classList.add('modal-image-card');
-  modalImageCard.addEventListener('click', function () {
-  modalImageCard.remove(); // При кліку на модальне вікно воно закриється
-  });
+// function zoomImage(image) {
+//   // Створюємо модальне вікно для збільшення картинки
+//   var modalImageCard = document.createElement('div');
+//   modalImageCard.classList.add('modal-image-card');
+//   modalImageCard.addEventListener('click', function () {
+//   modalImageCard.remove(); // При кліку на модальне вікно воно закриється
+//   });
 
-  // Створюємо зображення в модальному вікні
-  var modalImage = document.createElement('img');
-  modalImage.src = image.src;
-  modalImage.alt = image.alt;
-  modalImage.classList.add('modal-image');
+//   // Створюємо зображення в модальному вікні
+//   var modalImage = document.createElement('img');
+//   modalImage.src = image.src;
+//   modalImage.alt = image.alt;
+//   modalImage.classList.add('modal-image');
 
-  // Додаємо зображення в модальне вікно
-  modalImageCard.appendChild(modalImage);
+//   // Додаємо зображення в модальне вікно
+//   modalImageCard.appendChild(modalImage);
 
-  // Додаємо модальне вікно до body
-  document.body.appendChild(modalImageCard);
-}
+//   // Додаємо модальне вікно до body
+//   document.body.appendChild(modalImageCard);
+// }
 
-// Отримання всіх елементів зображень слайдера
-const sliderItems = document.querySelectorAll('.slider-item');
+// // Отримання всіх елементів зображень слайдера
+// const sliderItems = document.querySelectorAll('.slider-item');
 
-// Перехід до наступного зображення при кліку на поточне зображення
-sliderItems.forEach(item => {
-  item.addEventListener('click', () => {
-    // Вибір наступного елемента
-    const nextItem = item.nextElementSibling || sliderItems[0];
+// // Перехід до наступного зображення при кліку на поточне зображення
+// sliderItems.forEach(item => {
+//   item.addEventListener('click', () => {
+//     // Вибір наступного елемента
+//     const nextItem = item.nextElementSibling || sliderItems[0];
 
-    // Зміна стилів для показу наступного зображення
-    item.classList.remove('active');
-    nextItem.classList.add('active');
-  });
-});
+//     // Зміна стилів для показу наступного зображення
+//     item.classList.remove('active');
+//     nextItem.classList.add('active');
+//   });
+// });
