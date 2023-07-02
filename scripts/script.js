6cd2a130d006125519e20264827382207a9521b8
Yullia90 <y.tsarenko@harwind.com.ua>
2023-07-03 00:34:12 +0300

    add correct table

--- scripts/script.js
+++ scripts/script.js
@@ -22,14 +22,15 @@ for (let i = 0; i < buttons.length; i++) {
   };
 }
 
-//обращаемся к кажжому полю
-let burger = document.getElementById('burger');
+//обращаемся к каждому полю
+let sink = document.getElementById('sink');
 let name = document.getElementById('name');
 let phone = document.getElementById('phone');
+
 //делаем валидацию форми
 document.getElementById('order-action').onclick = function () {
   let hasError = false;
-  [burger, name, phone].forEach(item => {
+  [sink, name, phone].forEach(item => {
     if (!item.value) {
       item.parentElement.style.background = 'red';
       hasError = true;
@@ -39,42 +40,44 @@ document.getElementById('order-action').onclick = function () {
   });
   //проверяем на ошибки
   if (!hasError) {
-    [burger, name, phone].forEach(item => {
-      item.value = ' '; //чистим строку
+    [sink, name, phone].forEach(item => {
+      item.value =' '; //чистим строку
     });
     alert('Дякую за замовлення!');
   }
 };
 
-let prices = document.getElementsByClassName('products-item-price'); //поиск всех цен
 
-//меняем валюту
-document.getElementById('change-currency').onclick = function (e) {
-  let currentCurrency = e.target.innerText; //текущая валюта
-  //новая валюта на которую ми изменяем и коефициент по которому будем пересчитивать цену
-  let newCurrency = '$';
-  let coefficient = 1;
-  // будем определять какая сейчас валюта
-  if (currentCurrency === '$') {
-    newCurrency = '₴';
-    coefficient = 38;
-  } else if (currentCurrency === '₴') {
-    newCurrency = '€';
-    coefficient = 3;
-  } else if (currentCurrency === '€') {
-    newCurrency = '¥';
-    coefficient = 6.9;
-  } else if (currentCurrency === '¥') {
-    newCurrency = '£';
-    coefficient = 0.9;
-  }
-  e.target.innerText = newCurrency;
-  //перебираем циклом по каждому елемент и меняем каждое значение по каждому курсу
-  for (let i = 0; i < prices.length; i++) {
-    prices[i].innerText =
-      +(prices[i].getAttribute('data-base-price') * coefficient).toFixed(1) + ' ' + newCurrency;
-  }
-};
+
+// let prices = document.getElementsByClassName('products-item-price'); //поиск всех цен
+
+// //меняем валюту
+// document.getElementById('change-currency').onclick = function (e) {
+//   let currentCurrency = e.target.innerText; //текущая валюта
+//   //новая валюта на которую ми изменяем и коефициент по которому будем пересчитивать цену
+//   let newCurrency = '$';
+//   let coefficient = 1;
+//   // будем определять какая сейчас валюта
+//   if (currentCurrency === '$') {
+//     newCurrency = '₴';
+//     coefficient = 38;
+//   } else if (currentCurrency === '₴') {
+//     newCurrency = '€';
+//     coefficient = 3;
+//   } else if (currentCurrency === '€') {
+//     newCurrency = '¥';
+//     coefficient = 6.9;
+//   } else if (currentCurrency === '¥') {
+//     newCurrency = '£';
+//     coefficient = 0.9;
+//   }
+//   e.target.innerText = newCurrency;
+//   //перебираем циклом по каждому елемент и меняем каждое значение по каждому курсу
+//   for (let i = 0; i < prices.length; i++) {
+//     prices[i].innerText =
+//       +(prices[i].getAttribute('data-base-price') * coefficient).toFixed(1) + ' ' + newCurrency;
+//   }
+// };
 //Слайдер
 $(document).ready(function () {
   $('.slider').slick({
@@ -118,79 +121,6 @@ function topFunction() {
     document.documentElement.scrollTop = 0;
 }
 
-
-
-// var openButtons = document.querySelectorAll('.button-about');
-// var modals = document.querySelectorAll('.modal');
-
-// openButtons.forEach(function (button) {
-//   button.addEventListener('click', function () {
-//     var modalId = button.dataset.modalId;
-//     var modal = document.getElementById(modalId);
-//     modal.style.display = 'block';
-
-//     // Додати обробник подій для закриття модального вікна при натисканні на крестик
-//     var closeButtons = modal.getElementsByClassName('close');
-//     for (var i = 0; i < closeButtons.length; i++) {
-//       closeButtons[i].addEventListener('click', function () {
-//         modal.style.display = 'none';
-//       });
-//     }
-//   });
-// });
-
-// // Додати обробник подій для закриття модального вікна при натисканні на клавішу Esc
-// document.addEventListener('keydown', function (event) {
-//   if (event.key === 'Escape') {
-//     modals.forEach(function (modal) {
-//       modal.style.display = 'none';
-//     });
-//   }
-// });
-
-// // Додати обробник подій для закриття модального вікна при натисканні де-небудь на сторінці
-// document.addEventListener('click', function (event) {
-//   if (event.target.classList.contains('modal')) {
-//     event.target.style.display = 'none';
-//   }
-// });
-
-
-// function zoomImage(image) {
-//   // Створюємо модальне вікно для збільшення картинки
-//   var modalImageCard = document.createElement('div');
-//   modalImageCard.classList.add('modal-image-card');
-//   modalImageCard.addEventListener('click', function () {
-//   modalImageCard.remove(); // При кліку на модальне вікно воно закриється
-//   });
-
-//   // Створюємо зображення в модальному вікні
-//   var modalImage = document.createElement('img');
-//   modalImage.src = image.src;
-//   modalImage.alt = image.alt;
-//   modalImage.classList.add('modal-image');
-
-//   // Додаємо зображення в модальне вікно
-//   modalImageCard.appendChild(modalImage);
-
-//   // Додаємо модальне вікно до body
-//   document.body.appendChild(modalImageCard);
-// }
-
-// // Отримання всіх елементів зображень слайдера
-// const sliderItems = document.querySelectorAll('.slider-item');
-
-// // Перехід до наступного зображення при кліку на поточне зображення
-// sliderItems.forEach(item => {
-//   item.addEventListener('click', () => {
-//     // Вибір наступного елемента
-//     const nextItem = item.nextElementSibling || sliderItems[0];
-
-//     // Зміна стилів для показу наступного зображення
-//     item.classList.remove('active');
-//     nextItem.classList.add('active');
-//   });
-// });
 var openButtons = document.querySelectorAll('.button-about');
 var modals = document.querySelectorAll('.modal');
 
@@ -265,3 +195,4 @@ sliderItems.forEach(item => {
     nextItem.classList.add('active');
   });
 });
+
